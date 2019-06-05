77994b7b4664b008c55b02889b900b3927b0d97e
Florian <f.thery3103@gmail.com>
2019-06-06 00:00:39 +0200

    yaml exp

--- Program.cs
+++ Program.cs
@@ -1,13 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using Qml.Net;
 using Qml.Net.Runtimes;
+using YamlDotNet.Core;
+using YamlDotNet.Core.Events;
+using YamlDotNet.RepresentationModel;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
 
 namespace QmlnetExp
 {
     class Program
     {
-        public class NetObject
+      public class NetObject
         {
             public void Method()
             {
@@ -17,9 +24,27 @@ namespace QmlnetExp
 
         static int Main(string[] args)
         {
+            var unitsConfig = File.ReadAllText("res/config/units.yaml");
+            var buildingsConfig = File.ReadAllText("res/config/buildings.yaml");
+            string config = unitsConfig + "\n" + buildingsConfig;
             
+            var deserializer = new DeserializerBuilder()
+                .WithNamingConvention(new CamelCaseNamingConvention())
+                .Build();
+            
+            var widgets = deserializer.Deserialize<Dictionary<string,Dictionary<string,Widget>>>(config);
+
+            foreach (var item in widgets)
+            {
+                foreach (var widget in item.Value)
+                {
+                    Console.WriteLine(widget.Value);
+                }
+            }
+
+
+            /*
             RuntimeManager.DiscoverOrDownloadSuitableQtRuntime();
-            
             QQuickStyle.SetStyle("Material");
             using (var app = new QGuiApplication(args))
             {
@@ -30,6 +55,10 @@ namespace QmlnetExp
                     return app.Exec();
                 }
             }
+            */
+            return 0;
         }
     }
+    
+    
 }
